7c752f75471c183f4eca3a42f64e8809525577e6
DalerAlyamov <developer.daler@gmail.com>
2021-09-14 19:16:11 +0500

    .

---
+++ src/icons/CheckCircleOutline.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import SvgTemplate from './SvgTemplate'
+
+const Svg = styled(SvgTemplate)` 
+  width: ${props => props.size}px; 
+  height: ${props => props.size}px;
+`
+
+export const CheckCircleOutline = ({ 
+  className,
+  size=24
+}) => ( 
+  <Svg viewBox='0 0 24 24' className={className} size={size} fill='currentColor'>   
+    <path 
+      fill='currentColor'
+      d='M16.59 7.58L10 14.17L6.41 10.59L5 12L10 17L18 9L16.59 7.58ZM12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.58 20 4 16.42 4 12C4 7.58 7.58 4 12 4C16.42 4 20 7.58 20 12C20 16.42 16.42 20 12 20Z' 
+    />
+  </Svg>
+)
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App'
 import store from './redux/reducers' 
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
-import './sass/root.sass'
+import './sass/root.scss'
 
 ReactDOM.render(
   <Router basename='/'>
