e291c2c351dd3ea175fa370e50c0d6ad7c67d3c4
Amin444 <aminmirzoev123@gmail.com>
2021-09-15 18:40:20 +0500

    .

---
+++ .history/src/atoms/Icons_20210915182030.js
@@ -0,0 +1,28 @@
+import classNames from 'classnames'
+import React, { useState } from 'react'
+import styles from '../scss/atom/Icons.module.scss'
+
+const Icons = ({
+ icon,
+ clasName,
+ active,
+ rotate
+}) => {
+
+
+  return (
+    <div className={styles.root}>
+      <button
+        className={classNames(
+          clasName, 
+          rotate && styles.rotate,
+          !active && styles.dis
+        )}
+      >
+        {icon}
+      </button>
+    </div>
+  )
+}
+
+export default Icons
---
+++ .history/src/orgamisms/SideBar_20210915181042.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import styles from '../scss/organisms/Sidebar.module.scss'
+import { Menu } from '../icons/Menu';
+import Icon from '../atoms/Icons'
+const SideBar = () => {
+
+  return (
+    <div className={styles.root}>
+      <Icon
+       icon={<Menu size={40} />}
+       clasName={styles.icon}
+       active={true}
+       rotate={true}  
+      />
+    </div>
+  )
+}
+
+export default SideBar
