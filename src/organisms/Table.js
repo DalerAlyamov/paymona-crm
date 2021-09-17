75cab82789ec0dff2cbc3a524ff0dd80b94648c6
DalerAlyamov <developer.daler@gmail.com>
2021-09-17 19:12:25 +0500

    .

---
+++ src/organisms/Table.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import classNames from 'classnames'
+import styles from '../scss/organisms/Table.module.scss'
+
+const Table = ({
+  className
+}) => {
+  return (
+    <div className={classNames(className, styles.root)}>
+      
+    </div>
+  )
+}
+
+export default Table
--- src/organisms/index.js
+++ src/organisms/index.js
@@ -1,9 +1,11 @@
 import LoginPage from './LoginPage' 
 import LoginPanel from './LoginPanel' 
 import SideBar from './SideBar' 
+import Table from './Table' 
 
 export {
   LoginPage,
   LoginPanel,
-  SideBar
+  SideBar,
+  Table
 }
